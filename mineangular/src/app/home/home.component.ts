ea138743ce154a598af57f6a832319a1cafe98a4
fabio <fabio.petrone@outlook.it>
2021-10-31 09:50:51 +0100

    instance of navigationend x aggiornare a cambio rotta senza refresh

--- mineangular/src/app/home/home.component.ts
+++ mineangular/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
+import { NavigationStart, Router,Event, NavigationEnd} from '@angular/router';
 import { Observable } from 'rxjs';
 import { MainService } from '../mainservice.service';
 import { Book } from '../models/book.model';
@@ -13,21 +14,25 @@ import { Book } from '../models/book.model';
 export class HomeComponent implements OnInit {
   @Input() filteredBooks:Book[] = [];
   // books:Book[] = [];
-  constructor(public mservice:MainService,private title:Title, private meta:Meta) {
+  constructor(public mservice:MainService,private title:Title, private meta:Meta,private router:Router) {
   this.title.setTitle("Mine-angular");
   this.meta.updateTag({name:'description',content:"questa è la homepage"});
   this.meta.updateTag({name:'keywords',content:"homepage,landing,home"});
-  //     this.mservice.downloadBooks().subscribe(res=>{
-  //     this.books=res;
-  //     console.log(this.books);
-  // })
+  this.router.events.subscribe((event: Event) => {
+    if (event instanceof NavigationEnd) {
+     this.mservice.downloadBooks().subscribe(res=>{
+      this.filteredBooks=res;
+      console.log(this.filteredBooks);
+  })
+    }
+});
 }
 
   ngOnInit(): void {
+
     this.mservice.booksFiltered$.subscribe(books=>{
       this.filteredBooks=books;
     })
-    console.log(this.filteredBooks);
   //   this.mservice.downloadBooks().subscribe(res=>{
   //     this.filteredBooks=res;
   //     console.log(this.filteredBooks);
